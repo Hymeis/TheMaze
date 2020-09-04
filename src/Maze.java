4209756d06f99e879d4d9db9ed9397f42ab6c321
Andy Qin <56485866+AndyQYH@users.noreply.github.com>
2020-09-04 14:40:12 -0700

    Revert "change to Board.java"
    This reverts commit 0b8e7c42ecc15778430dc5e6cf9a9c1bc8b6bfc8.

--- src/Maze.java
+++ src/Maze.java
@@ -1,7 +1,6 @@
 
 public class Maze extends Board{
 	
-	//private int[][] newmaze;
 	public Maze() {
 		super();
 	}
@@ -18,36 +17,29 @@ public class Maze extends Board{
 		nextStep(0,0);
 	}
 	
-	public boolean hasDefaultAround(int width, int length) {
-		return (getGridValue(width-2,length) == DEFAULT || getGridValue(width+2,length) == DEFAULT||
-			   getGridValue(width,length-2) == DEFAULT || getGridValue(width,length+2) == DEFAULT);
+	private boolean hasDefaultAround(int width, int length) {
+		return getGridValue(width-1,length) == DEFAULT || getGridValue(width+1,length) == DEFAULT ||
+			   getGridValue(width,length-1) == DEFAULT || getGridValue(width,length+1) == DEFAULT ;
 	}
 	
 	private void nextStep(int width, int length) {
-		
+		int randomDirection = UP + (int)(4*Math.random());
 		setGridValue(width,length,GENERATING);
-		
-		while (hasDefaultAround(width, length) == true) {
-			int randomDirection = UP + (int)(4*Math.random());
-			System.out.println("direction: " + randomDirection);
-			if (randomDirection == UP && getGridValue(width-2,length) == DEFAULT) {
-				nextStep(width-2,length);
-			} else if (randomDirection == LEFT && getGridValue(width,length-2) == DEFAULT) {
-				nextStep(width,length-2);
-			} else if (randomDirection == DOWN && getGridValue(width+2,length) == DEFAULT) {
-				nextStep(width+2,length);
-			} else if (randomDirection == RIGHT && getGridValue(width,length+2) == DEFAULT) {
-				nextStep(width,length+2);
+		while (hasDefaultAround(width, length)) {
+			if (randomDirection == UP && getGridValue(width-1,length) == DEFAULT) {
+				nextStep(width-1,length);
+			} else if (randomDirection == LEFT && getGridValue(width,length-1) == DEFAULT) {
+				nextStep(width,length-1);
+			} else if (randomDirection == DOWN && getGridValue(width+1,length) == DEFAULT) {
+				nextStep(width+1,length);
+			} else if (randomDirection == RIGHT && getGridValue(width+1,length) == DEFAULT) {
+				nextStep(width,length+1);
 			}
 			// If none of the conditions is triggered, that means the grid @ randomized direction does not have default value
 			/* Set up walls to finalize the maze Below */
 			
 		}
 
-	}
-	
-	public void printMaze() {
-		super.printBoard();
 	}
 
 }
--- src/tester.java
+++ src/tester.java
@@ -1,55 +1,10 @@
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
 
-public class tester extends JPanel{
-	  private static final int RECT_X = 100;
-	  private static final int RECT_Y = RECT_X;
-	  private static final int RECT_WIDTH = 100;
-	  private static final int RECT_HEIGHT = RECT_WIDTH;
-	  
-	  @Override
-	   protected void paintComponent(Graphics g) {
-	      super.paintComponent(g);
-	      // draw the rectangle here
-	      g.drawRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
-	      g.setColor(java.awt.Color.blue);
-	      g.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
-	   }
+public class tester {
 
-	   @Override
-	   public Dimension getPreferredSize() {
-	      // so that our GUI is big enough
-	      return new Dimension(RECT_WIDTH + 2 * RECT_X, RECT_HEIGHT + 2 * RECT_Y);
-	   }
-	   
-	   private static void createAndShowGui() {
-		   tester mainPanel = new tester();
-
-		   JFrame frame = new JFrame("DrawRect");
-		   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		   frame.getContentPane().add(mainPanel);
-		   frame.pack();
-		   frame.setLocationByPlatform(true);
-		   frame.setVisible(true);
-		   
-		   }
-
-	   public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Board a = new Board();
-		Maze b = new Maze(4,4);
-		b.generateMaze();
-		int path = a.getGridValue(0 - 2,0);
-		boolean hasdefault = b.hasDefaultAround(6,0);
-		//createAndShowGui();
+		Board a = new Board(20,12);
 		a.printBoard();
-		System.out.println();
-		System.out.println(path);
-		System.out.println(hasdefault);
-		System.out.println();
-		b.printMaze();
-		
 	}
 
 }
